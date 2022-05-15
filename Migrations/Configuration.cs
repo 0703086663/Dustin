dcee9e859259acee226ce451bedcd44230973c3a
Tien Thanh <nguyentienthanh.tgdd@gmail.com>
2022-05-16 01:18:22 +0700

    Add project files.

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Dustin.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Dustin.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Dustin.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Dustin.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}

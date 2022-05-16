97169b8ee406860327e430bc93214db7f1d20e2a
Tien Thanh <nguyentienthanh.tgdd@gmail.com>
2022-05-17 00:35:09 +0700

    Add Staff Area

---
+++ Areas/Staff/StaffAreaRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace Dustin.Areas.Staff
+{
+    public class StaffAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Staff";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            //context.MapRoute(
+            //    "Staff_default",
+            //    "Staff/{controller}/{action}/{id}",
+            //    new { action = "Index", id = UrlParameter.Optional }
+            //);
+            context.MapRoute(
+                "Staff_default",
+                "Staff/{controller}/{action}/{id}",
+                new { action = "Index", controller = "Home", id = UrlParameter.Optional },
+                new[] { "Dustin.Areas.Staff.Controllers" }
+            );
+        }
+    }
+}
---
+++ Models/Brand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Dustin.Models
+{
+    public class Brand
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Desc { get; set; }
+    }
+
+    public class BrandDbContext : DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
